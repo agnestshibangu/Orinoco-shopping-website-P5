3009e3394f7102bacad8b043e04ba34591e6324f
Agnes <agnes.tshibangugrillon@gmail.com>
2021-08-25 02:54:51 +0200

    first commit front part

---
+++ src/app-product.js
@@ -0,0 +1,141 @@
+let url_product_id = new URLSearchParams(window.location.search);
+let _id = url_product_id.get("id");
+
+fetch('http://localhost:3000/api/cameras/'+ _id)
+// fetch('http://localhost:3000/api/cameras/5be1ef211c9d44000030b062')
+
+
+.then((resp) => resp.json())
+.then(function (data) {
+    infoProduct(data);
+
+    
+})
+
+// display num of items in cart
+let cartNumber = localStorage.getItem('cartNumber');
+productNumber = parseInt(cartNumber);
+document.getElementById('spanCartNb').innerText = productNumber
+
+function infoProduct(data) {
+
+    var name = data.name; 
+    var price = data.price/100 + '€';
+    var description = data.description;
+    var imageUrl = data.imageUrl;
+
+    document.getElementById("product-title").innerHTML = name;
+    document.getElementById("price").innerHTML = price;
+    document.getElementById("imgSrc").src = imageUrl;
+    document.getElementById("description").innerHTML = description;
+    
+    }
+
+
+    function shopButtonClicked() {
+        var addButtonShop = document.getElementsByClassName('shop-item-button')
+        for (var i = 0; i < addButtonShop.length; i++) {
+       
+        var button = addButtonShop[i]
+    
+        addToCartClicked()
+        button.innerHTML = "IN CART"
+        button.style.backgroundColor = "yellow"
+
+        }
+   }
+
+
+
+ 
+
+   
+    function addToCartClicked() {
+
+    
+       console.log('button SHOP clicked');
+       alert("You've added 1 item to the cart ")
+
+       
+       const button = event.target;
+       var shopItem = button.parentElement;
+       var name = document.getElementById('product-title').innerText;
+       var price = document.getElementById('price').innerText;
+       var imageUrl = document.getElementById('imgSrc').src;
+       /// get the item id 
+       var url = window.location;
+       let params = (new URL(url)).searchParams; 
+       let id = params.get('id'); 
+       console.log(id)
+       /// get the choosen option
+       var option = document.getElementById("lense-select").value;
+       var quantity = 1;
+
+       var objItem = {
+         id : id,
+         name : name, 
+         quantity : quantity,
+         price : price, 
+         description : option,
+         imageUrl : imageUrl, 
+        }
+
+      // on passe l'objet en string 
+      const newData = JSON.stringify(objItem)  
+       
+      // on recupère la liste d'items qui est stockée dans le localstorage
+      let oldData = JSON.parse(localStorage.getItem('data')) || [];
+
+
+      console.log(oldData)
+      
+      var item = oldData.find(item => item.id === id);
+    
+      if (item) {
+
+          alert ('item already in cart')
+
+      } else if (item == undefined) {
+
+        console.log('old data inf to 1 ')
+        oldData.push(newData)
+        console.log(oldData)
+        localStorage.setItem('data', JSON.stringify(oldData))
+    }
+
+}
+
+
+
+
+
+
+
+
+function cartNumbers() {
+    
+   let productNumbers = localStorage.getItem('cartNumber');
+   
+   productNumbers = parseInt(productNumbers);
+  
+
+   if (productNumbers) {
+       localStorage.setItem('cartNumber', productNumbers +1);
+       document.getElementById('spanCartNb').innerText = productNumbers +1 ;
+    
+   } else {
+       localStorage.setItem('cartNumber', 1);
+       document.getElementById('spanCartNb').innerText = 1;
+   }
+//    console.log(productNumbers);
+   
+}
+
+
+
+
+
+
+
+
+
---
+++ src/app.js
@@ -0,0 +1,37 @@
+fetch('http://localhost:3000/api/cameras')
+.then((resp) => resp.json())
+.then(function (data) {
+    showCameras(data);
+    
+
+})
+
+// display num of items in cart
+let cartNumber = localStorage.getItem('cartNumber');
+productNumber = parseInt(cartNumber);
+document.getElementById('spanCartNb').innerText = productNumber
+
+function showCameras(data) {
+
+    console.log(data);
+
+    var mainContainer = document.getElementById("container");
+    
+    for (var i = 0; i < data.length; i++) {
+        const card = `
+        <div class="card col-sm-12 col-4  mx-auto products" style="width: 18rem;">
+            <img src="${data[i].imageUrl}" class="card-img-top" alt="${data[i].name}">
+        <div class="card-body">
+            <h5 class="card-title">${data[i].name}</h5>
+            <h5 class="card-price">${data[i].price/100}€</h5>
+            <p class="card-text">${data[i].description}</p>
+            <a href="product.html?id=${data[i]._id}" class="btn-shop link-id">Details</a>
+
+        </div>
+        </div>`;
+
+      var container = document.getElementById("myData");
+     
+      document.getElementById("myData").innerHTML += card;      
+    }
+}
